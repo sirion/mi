fb2fabc1e5aa0ad5ab649738313b62ff97f7c10e
Jens Himmelrath <jens@dev.himmelrath.net>
2022-08-16 15:45:44 +0200

    [rs] move rust crate to smaller units

--- rust/src/http/traits.rs
+++
@@ -1,8 +0,0 @@
-/// A combination of matcher and handler function for [super::Request]s. A standard implementation is available with [super::Handler].
-pub trait RequestHandler: Sync + Send {
-	/// Returns true if the handling method whould be called for the given request
-	fn matches(&self, req: &super::Request) -> bool;
-
-	/// Is called for the given request if it is the first Requesthandler that matches the request
-	fn handle(&self, req: &super::Request, res: super::Response);
-}
